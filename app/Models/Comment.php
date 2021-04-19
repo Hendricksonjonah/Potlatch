f8dbed86c4b8e83fcd071e90b0e11c3989459820
Raknerud <49953077+Raknerud@users.noreply.github.com>
2021-04-19 17:01:00 -0500

    Comment Model
    Added Comment Model

---
+++ app/Models/Comment.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use CodeIgniter\Model;
+
+class Comment extends Model {
+    protected $table = 'comment';
+    protected $primaryKey = 'id';
+    protected $useAutoIncrement = true;
+    protected $returnType = 'array';
+
+    protected $allowedFields = ['reply_id', 'item_id', 'user_id', 'comment', 'timestamp'];
+}
+
+?>
